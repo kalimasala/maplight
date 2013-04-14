bdbd6f34831c7e6b608d81b3e4ab9357bb66c87d
Ramki Venkatachalam <ramki@whendor.com>
2013-04-13 18:46:59 -0700

    fixing CSV download

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,34 +13,18 @@ public class Application extends Controller {
         render();
     }
     
-    public static void listByRecipientDonorYear(String recipient, String donor, int year,
-    		boolean download) {
-    	recipient = recipient.toLowerCase();
-    	donor = donor.toLowerCase();
-    	if (recipient.isEmpty() && donor.isEmpty()) {
-    		flash.error("both recipient and donor cannot be empty");
-    		index();
-		}
-    	List <CandidateContributions> cc = null;
-    	if (!recipient.isEmpty() && !donor.isEmpty()) {
-    		cc = CandidateContributions.findByRecipientDonor(recipient, donor, year);
-        } else if (!recipient.isEmpty()) {
-    		cc = CandidateContributions.findByRecipient(recipient, year);
-    	} 
-    	else {
-    		cc = CandidateContributions.findByDonor(donor, year);
-    	}
-    	if (download) {
-    		response.setHeader("Content-Disposition", "attachment; filename=download.csv");
-    		renderTemplate("Application/listByRecipientDonorYear.csv", cc);
-    	}
-    	else {
-        	render(cc, recipient, donor, year);
-    	}
-    }
-    
-  public static void byDonors() {
+  public static void byDonors(String donor, String recipient, String date_start, String date_end,
+		  String location_from, String location_to, boolean download) {
 	List<CandidateContributions> cc = CandidateContributions.get(params);
-	renderTemplate("Application/CandidateContributions.html", cc);
+
+	if (download) {
+		response.setHeader("Content-Disposition", "attachment; filename=download.csv");
+		renderTemplate("Application/CandidateContributions.csv", cc);
+	}
+	else {
+		renderTemplate("Application/CandidateContributions.html", cc, donor, recipient, date_start, 
+				date_end, location_from, location_to);
+	}
   }
+  	
 }
